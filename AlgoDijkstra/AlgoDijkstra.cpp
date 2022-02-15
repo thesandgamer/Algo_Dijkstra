1042845eb5959fd771cb6f9c47065a37755a5a51
etudiant <etudiant@md12-2021-004>
2022-02-15 12:54:30 +0100

    Ajoutez des fichiers projet.

---
+++ AlgoDijkstra/AlgoDijkstra.cpp
@@ -0,0 +1,29 @@
+// AlgoDijkstra.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
+//
+// Trouve dans la liste le node avec le nom qu'on veut et on le set en node de départ
+// 
+//
+// Find: operator qui va comparer deux nodes avec leurs paralètres
+
+
+struct Node
+{
+    char node;
+    vector<Node> connectedNodes;
+    int cost;
+
+};
